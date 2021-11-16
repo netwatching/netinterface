e60a33f0b82a318e35e93fe7d31c4c6f59101d90
Paul Zoppoth <zoppothp@edu.htl-villach.at>
2021-11-16 11:03:18 +0100

    implemented unauthorized login page redirect

--- net-interface/src/app/app-routing.module.ts
+++ net-interface/src/app/app-routing.module.ts
@@ -3,9 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { DeviceComponent } from 'src/_components/device/device.component';
 import { HomeComponent } from 'src/_components/home/home.component';
 import { AppComponent } from './app.component';
+import { AuthService } from '../_services/auth.service';
+import { AuthGuard } from './auth.guard';
+
+authService: AuthService
+
+
 
 const routes: Routes = [
-  { path: 'device', component: DeviceComponent },
+
+  { path: 'device', component: DeviceComponent, canActivate: [AuthGuard]},
   { path: '', component: HomeComponent },
 ];
 
---
+++ net-interface/src/app/auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../_services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable < boolean > | Promise < boolean > | boolean {
+
+    if (!this.authService.authenticated) {
+      this.router.navigate(['/home']);
+      return false;
+    }
+    return true;
+
+  }
+}
