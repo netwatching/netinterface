953a0eefa953c3e226fdcb4b12590e3dbac49017
Paul Zoppoth <zoppothp@edu.htl-villach.at>
2021-11-29 16:45:29 +0100

    implemented access token refresh

--- net-interface/src/_interceptors/jwt.interceptor.ts
+++ net-interface/src/_interceptors/jwt.interceptor.ts
@@ -5,7 +5,7 @@ import { CentralApiService } from '../_services/central-api.service';
 import { User } from '../_interfaces/user';
 import { AuthService } from '../_services/auth.service';
 import { Jwt } from '../_interfaces/jwt';
-import {JwtHelperService} from '@auth0/angular-jwt';
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 const TOKEN_HEADER_KEY = 'Authorization';
 
@@ -14,52 +14,41 @@ export class JwtInterceptor implements HttpInterceptor {
   login_url = 'http://palguin.htl-vil.local:8081/api/login'
   refresh_url = 'http://palguin.htl-vil.local:8081/api/refresh'
   jwt!: Jwt;
-  jwtHelperService!: JwtHelperService;
-
-  isRefresh!: Boolean;
+  helper = new JwtHelperService();
 
   constructor(public centralApiService: CentralApiService, public authService: AuthService) {}
 
-
-
   intercept(request: HttpRequest < unknown > , next: HttpHandler): Observable < HttpEvent < unknown >> {
     let auth_request = request;
+    const requestData: any = {};
+    requestData['id'] = this.authService.user!.id;
+    requestData['name'] = this.authService.user!.username
+    requestData['pw'] = 'TYlZfng0wwuEOaxcyyoJ2N5otTPS0g4X6fXq9s777yJxwtcpHsRQC1F5Ao5PI3MT42xlMeBOP4jN7fUAA5a5vEtM7WWIMYvQPDebr5Lcgz9Ri1yEQiwmObINIHyI8pMw'
 
-    console.log(String(this.jwtHelperService.isTokenExpired(localStorage.getItem('access_token')?.toString())))
-    // if (this.jwtHelperService.isTokenExpired(String(localStorage.getItem('access_token')))) {
-    if (!localStorage.getItem('access_token') && !request.url.startsWith(this.login_url)) {
-      const requestData: any = {};
-      requestData['id'] = this.authService.user!.id
-      requestData['name'] = this.authService.user!.username
-      requestData['pw'] = 'TYlZfng0wwuEOaxcyyoJ2N5otTPS0g4X6fXq9s777yJxwtcpHsRQC1F5Ao5PI3MT42xlMeBOP4jN7fUAA5a5vEtM7WWIMYvQPDebr5Lcgz9Ri1yEQiwmObINIHyI8pMw'
-      this.centralApiService.getJWTToken(requestData).subscribe((object) => {
-        localStorage.setItem('refresh_token', object.refresh_token);
-        localStorage.setItem('access_token', object.access_token);
-      });
+    console.log('des is da access token: ', localStorage.getItem('access_token'))
+    console.log("is expired: ", this.helper.isTokenExpired(localStorage.getItem('access_token')?.toString()))
+
+    if (!this.helper.isTokenExpired(localStorage.getItem('access_token')?.toString()) || !localStorage.getItem('access_token')) {
+      console.log("nix expired")
+      if (!localStorage.getItem('access_token') && !request.url.startsWith(this.login_url)) {
+        this.centralApiService.getJWTToken(requestData).subscribe((object) => {
+          localStorage.setItem('refresh_token', object.refresh_token);
+          localStorage.setItem('access_token', object.access_token);
+        });
+      }
+    } else {
+        console.log("token expired")
+        this.centralApiService.getJWTToken(requestData).subscribe((object) => {
+          localStorage.setItem('refresh_token', object.refresh_token);
+          localStorage.setItem('access_token', object.access_token);
+        });
     }
-    // } else {
-    //   console.log("token expired")
-    //   if (!request.url.startsWith(this.refresh_url)) {
-    //     this.isRefresh = true
-    //     this.centralApiService.getNewAccessToken().subscribe((object) => {
-    //       localStorage.setItem('access_token', object.access_token);
-    //     });
-    //   }
-    // }
-
 
     if (this.authService.authenticated) {
-      if (!this.isRefresh) {
         auth_request = request.clone({
           headers: request.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + localStorage.getItem('access_token'))
         });
-      } else {
-        auth_request = request.clone({
-          headers: request.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + localStorage.getItem('refresh_token'))
-        });
-      }
     }
-
     return next.handle(auth_request);
   }
 }
