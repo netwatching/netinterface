8e0bae5d94d48ec25f89969584daaf9ecb79e1eb
Paul Zoppoth <zoppothp@edu.htl-villach.at>
2021-12-11 14:39:36 +0100

    changed redirect URI for MS OAuth

--- net-interface/src/_interfaces/oauth.ts
+++ net-interface/src/_interfaces/oauth.ts
@@ -1,6 +1,7 @@
 export const OAuthSettings = {
   appId: 'a3a1423c-deca-4367-b276-55d42b0d120b',
-  redirectUri: 'http://localhost:4200',
+  // redirectUri: 'http://localhost:4200',
+  redirectUri: 'https://palguin.htl-vil.local:4200/',
   authority: 'https://login.microsoftonline.com/2b197efa-8e1b-4680-b263-8e237889b5b3',
   scopes: [
     "user.read",
