adf8abce75587d71d6bbbf10e05175ff1434ca44
Paul Zoppoth <zoppothp@edu.htl-villach.at>
2022-03-14 11:46:03 +0100

    minor changes

--- net-interface/src/_interceptors/jwt.interceptor.ts
+++ net-interface/src/_interceptors/jwt.interceptor.ts
@@ -12,7 +12,7 @@ const TOKEN_HEADER_KEY = 'Authorization';
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
   // login_url = 'http://palguin.htl-vil.local:8080/api/login'
-  login_url = 'https://palng seguin.htl-vil.local:8443/api/login'
+  login_url = 'https://palguin.htl-vil.local:8443/api/login'
   jwt!: Jwt;
   helper = new JwtHelperService();
 
@@ -21,12 +21,12 @@ export class JwtInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest < unknown > , next: HttpHandler): Observable < HttpEvent < unknown >> {
     let auth_request = request;
     const requestData: any = {};
-    requestData['id'] = "zoppothp"; //this.authService.user!.id
-    requestData['name'] = "zoppothp"; //this.authService.user!.username;
+    requestData['id'] = this.authService.user!.id;
+    requestData['name'] = this.authService.user!.username;
     requestData['pw'] = 'TYlZfng0wwuEOaxcyyoJ2N5otTPS0g4X6fXq9s777yJxwtcpHsRQC1F5Ao5PI3MT42xlMeBOP4jN7fUAA5a5vEtM7WWIMYvQPDebr5Lcgz9Ri1yEQiwmObINIHyI8pMw';
 
-    // console.log('current access token: ', localStorage.getItem('access_token'))
-    // console.log('is expired: ', this.helper.isTokenExpired(localStorage.getItem('access_token')?.toString()))
+    console.log('current access token: ', localStorage.getItem('access_token'))
+    console.log('is expired: ', this.helper.isTokenExpired(localStorage.getItem('access_token')?.toString()))
 
     if (!this.helper.isTokenExpired(localStorage.getItem('access_token')?.toString()) || !localStorage.getItem('access_token')) {
       if (!localStorage.getItem('access_token') && !request.url.startsWith(this.login_url)) {
